217a800ffee04d9a9ea68b4d18c6e055efe7dfe0
serge <serge@netbook>
2012-05-01 15:02:07 +0700

    russian translation added

---
+++ translations/guzum_ru.ts
@@ -0,0 +1,232 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="14"/>
+        <source>About Guzum</source>
+        <translation>О программе Guzum</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="42"/>
+        <source>Guzum</source>
+        <translation>Guzum</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="49"/>
+        <source>VERSION</source>
+        <translation>VERSION</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="86"/>
+        <source>Notification area icon and simple GnuGP-encrypted text files editor.</source>
+        <translation>Иконка в панели уведомлений и простой редактор текстов, зашифрованных в GnuPG.</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="96"/>
+        <source>Author: Sergei Stolyarov</source>
+        <translation>Автор: Сергей Столяров</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="105"/>
+        <source>Website:</source>
+        <translation>Вебсайт:</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="112"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;DejaVu Sans&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a href=&quot;https://bitbucket.org/cancel/guzum/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;https://bitbucket.org/cancel/guzum/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="24"/>
+        <source>version %1</source>
+        <translation>версия %1</translation>
+    </message>
+</context>
+<context>
+    <name>EncryptedTextWindow</name>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="46"/>
+        <source>&amp;Quit</source>
+        <translation>В&amp;ыход</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="47"/>
+        <source>&amp;About Guzum</source>
+        <translation>&amp;О программе</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="48"/>
+        <source>&amp;Save file</source>
+        <translation>&amp;Сохранить файл</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="49"/>
+        <source>Change current font</source>
+        <translation>Изменить &amp;шрифт</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="50"/>
+        <source>Set default &amp;font</source>
+        <translation>Изменить шрифт по &amp;умолчанию</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="76"/>
+        <source>&amp;File</source>
+        <translation>&amp;Файл</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="80"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Справка</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="138"/>
+        <location filename="../src/encryptedtextwindow.cpp" line="235"/>
+        <source>GPG_ERR_INV_VALUE</source>
+        <translation>GPG_ERR_INV_VALUE</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="143"/>
+        <source>GPG_ERR_NO_DATA</source>
+        <translation>GPG_ERR_NO_DATA</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="148"/>
+        <source>File is too large to decrypt.</source>
+        <translation>Не получается расшифровать: файл слишком большой.</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="153"/>
+        <source>Unable to decrypt message, most probably private key for the encrypted file has not been found.</source>
+        <translation>Не получается расшифровать, скорее всего, в хранилище нет нужного приватного ключа.</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="158"/>
+        <source>Incorrect passphrase.</source>
+        <translation>Неверная парольная фраза.</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="170"/>
+        <location filename="../src/encryptedtextwindow.cpp" line="242"/>
+        <source>Decryption failed</source>
+        <translation>Не удалось расшифровать</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="211"/>
+        <source>Data too large to encrypt.</source>
+        <translation>Слишком много данных для зашифровки.</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="215"/>
+        <source>Cannot find public key to encrypt data.</source>
+        <translation>Не удалось найти публичный шрифт для зашифровки данных.</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="219"/>
+        <source>Cannot open target file.</source>
+        <translation>Не удалось открыть нужный файл.</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="223"/>
+        <source>Cannot backup encrypted file, it&apos;s too large.</source>
+        <translation>Не удалось сделать резервную копию файла: он слишком большой.</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="227"/>
+        <source>More than one key found for key ID “%1”</source>
+        <translation>Для ID ключа «%1» нашлось больше одного ключа</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="231"/>
+        <source>Unusable public key (maybe expired or revoked)</source>
+        <translation>Публичный ключ нельзя использовать (он либо устарел, либо отозван)</translation>
+    </message>
+    <message>
+        <location filename="../src/encryptedtextwindow.cpp" line="239"/>
+        <source>Unknown error: %1</source>
+        <translation>Неизвестная ошибка: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="67"/>
+        <location filename="../src/main.cpp" line="80"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="68"/>
+        <source>File `%1&apos; not found</source>
+        <translation>File «%1» не найден</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="81"/>
+        <source>Cannot initialize GPG backend</source>
+        <translation>Не удалось инициализировать GPG</translation>
+    </message>
+</context>
+<context>
+    <name>PassphraseDialog</name>
+    <message>
+        <location filename="../src/passphrasedialog.ui" line="14"/>
+        <source>Enter passphrase for the key</source>
+        <translation>Введите парольную фразу для ключа</translation>
+    </message>
+    <message>
+        <location filename="../src/passphrasedialog.ui" line="20"/>
+        <source>UID:</source>
+        <translation>ID пользователя</translation>
+    </message>
+    <message>
+        <location filename="../src/passphrasedialog.ui" line="33"/>
+        <source>DEFAULT VALUE</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/passphrasedialog.ui" line="89"/>
+        <source>Passphrase is incorrect!</source>
+        <translation>Парольная фраза не подходит!</translation>
+    </message>
+</context>
+<context>
+    <name>PrefsDialog</name>
+    <message>
+        <location filename="../src/prefsdialog.ui" line="14"/>
+        <source>Guzum preferences</source>
+        <translation>Настройки Guzum</translation>
+    </message>
+    <message>
+        <location filename="../src/prefsdialog.ui" line="20"/>
+        <source>Start Guzum on system startup</source>
+        <translation>Запускать программу при старте системы</translation>
+    </message>
+    <message>
+        <location filename="../src/prefsdialog.ui" line="64"/>
+        <source>Cannot configure auto startupt feature, possible your system doesn&apos;t support that.</source>
+        <translation>Не удалось это опцию сконфигурировать, возможно, ваша система такого не поддерживает.</translation>
+    </message>
+</context>
+<context>
+    <name>TrayManager</name>
+    <message>
+        <location filename="../src/traymanager.cpp" line="36"/>
+        <source>&amp;Preferences…</source>
+        <translation>&amp;Настройки…</translation>
+    </message>
+    <message>
+        <location filename="../src/traymanager.cpp" line="37"/>
+        <source>&amp;About Guzum</source>
+        <translation>&amp;О программе</translation>
+    </message>
+    <message>
+        <location filename="../src/traymanager.cpp" line="38"/>
+        <source>&amp;Quit Guzum</source>
+        <translation>В&amp;ыход</translation>
+    </message>
+</context>
+</TS>
